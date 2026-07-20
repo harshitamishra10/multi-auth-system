a8692a85b763f29979ef98bb490be4d8c6ce139c
harshita <mishraharshitaims25@gmail.com>
2026-07-20 18:17:13 +0530

    chanhges

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,16 @@
+// // import axios from "axios";
+// import API from "../api/axios";
+// const API = axios.create({
+//   baseURL: "https://mern-auth-system-i4go.onrender.com/api/auth",
+//   withCredentials: true,
+// });
+
+// export default API;
+
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://mern-auth-system-i4go.onrender.com/api/auth",
+  baseURL: "https://mern-auth-system-beta.vercel.app/api/auth",
   withCredentials: true,
 });
 
